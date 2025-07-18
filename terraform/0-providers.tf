88a79dc954dbfb537bcc608f82e5a139d4661798
rob barker <neutronrabbit@gmail.com>
2025-07-18 12:10:17 +0100

    correct bucket name

--- terraform/0-providers.tf
+++ terraform/0-providers.tf
@@ -13,10 +13,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "sandbox-terraform-state-bucket"
+    bucket         = "neutronrabbit-tf-state-bucket"
     key            = "TF_State/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "tf-lock-table"
     encrypt        = true
   }
 }
