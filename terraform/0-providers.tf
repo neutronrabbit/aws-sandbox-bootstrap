0792452fd2568f3e20c0849e23f84c5be6465557
rob barker <neutronrabbit@gmail.com>
2025-07-18 12:07:23 +0100

    update with TF state in AWS S3

--- terraform/0-providers.tf
+++ terraform/0-providers.tf
@@ -1,15 +1,22 @@
 provider "aws" {
-	region = var.region
+  region = var.region
 }
 
 terraform {
-	required_version = ">=v1.12.2"
+  required_version = ">=v1.12.2"
 
-	required_providers {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+        version = ">= 6.3.0"
+    }
+  }
 
-		aws = {
-			source = "hashicorp/aws"
-			version = ">= 6.3.0"
-		}
-	}
+  backend "s3" {
+    bucket         = "sandbox-terraform-state-bucket"
+    key            = "TF_State/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "tf-lock-table"
+    encrypt        = true
+  }
 }
